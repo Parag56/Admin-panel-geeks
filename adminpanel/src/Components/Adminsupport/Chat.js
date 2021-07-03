78d6340b20fd52d4811603be97d9dafb65f73622
parag56 <paragthakur56@gmail.com>
2021-07-03 23:29:19 +0530

    chat feature

--- adminpanel/src/Components/Adminsupport/Chat.js
+++ adminpanel/src/Components/Adminsupport/Chat.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useCallback } from 'react'
 import $ from 'jquery'
+import { io } from "socket.io-client";
 import './Chat.css'
  const Chat = () => {
     const [roomids,setroomids]=useState()
     const [socket,setsocket]=useState(null)
+	const [activeroomid,setactiveroomid]=useState(null)
+	const [messages,setmessages]=useState([])
     useEffect(()=>{
         const url=`${process.env.REACT_APP_BACKEND_URL}getrooms/${JSON.parse(localStorage.getItem('adminData')).adminid}`
         console.log(url)
@@ -20,14 +23,47 @@ import './Chat.css'
             }
          })
     },[])
-    const handlesendmessage=()=>{
+	const addMsg=(msg) =>{
+		setmessages([...messages,{msg,id:JSON.parse(localStorage.getItem('adminData')).adminid}])
+		document.getElementById("message").value = "";
+		document.getElementById("chat").scrollTop = document.getElementById(
+		  "chat"
+		).scrollHeight;
+	}
 
+	const handlesendmessage=()=> {
+		var msg = document.getElementById("message").value;
+		if(socket&&activeroomid)
+		socket.emit('message-admin',msg,activeroomid,JSON.parse(localStorage.getItem('adminData')).adminid)
+		if (msg == "") return;
+		addMsg(msg);
+	  }
+	
+	  const handleenter=(event)=>{
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            handlesendmessage();
+          }
+       }
+    const handleconnection=(roomid)=>{
+		const socket=io("http://localhost:5000/connection")
+		socket.emit("join-room-admin",roomid)
+		setactiveroomid(roomid)
+		setsocket(socket)
     }
-    const handleconnection=()=>{
 
-    }
+		if(socket){
+       socket.on('message',(msg,id)=>{
+		   setmessages([...messages,{msg,id}])
+	   })
+	}
+	
+   
     return (    
 <div>
+{!roomids&&(
+	<div>There are no users</div>
+)}
 {roomids&&(
 <div id="container">
 	<aside>
@@ -36,7 +72,7 @@ import './Chat.css'
 		</header>
 		<ul>
             {roomids.map((id)=>{
-              return  <li onClick={handleconnection}>
+              return  <li onClick={()=>{handleconnection(id)}}>
                     <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_01.jpg" alt=""/>
 				<div>
 					<h2>Prénom Nom</h2>
@@ -58,76 +94,22 @@ import './Chat.css'
 			</div>
 			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_star.png" alt=""/>
 		</header>
-		<ul id="chat">
-			<li class="you">
-				<div class="entete">
-					<span class="status green"></span>
-					<h2>Vincent</h2>
-					<h3>10:12AM, Today</h3>
-				</div>
-				<div class="triangle"></div>
-				<div class="message">
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-				</div>
-			</li>
-			<li class="me">
-				<div class="entete">
-					<h3>10:12AM, Today</h3>
-					<h2>Vincent</h2>
-					<span class="status blue"></span>
-				</div>
-				<div class="triangle"></div>
-				<div class="message">
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-				</div>
-			</li>
-			<li class="me">
-				<div class="entete">
-					<h3>10:12AM, Today</h3>
-					<h2>Vincent</h2>
-					<span class="status blue"></span>
-				</div>
-				<div class="triangle"></div>
-				<div class="message">
-					OK
-				</div>
-			</li>
-			<li class="you">
-				<div class="entete">
-					<span class="status green"></span>
-					<h2>Vincent</h2>
-					<h3>10:12AM, Today</h3>
-				</div>
-				<div class="triangle"></div>
-				<div class="message">
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-				</div>
-			</li>
-			<li class="me">
-				<div class="entete">
-					<h3>10:12AM, Today</h3>
-					<h2>Vincent</h2>
-					<span class="status blue"></span>
-				</div>
-				<div class="triangle"></div>
-				<div class="message">
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-				</div>
-			</li>
-			<li class="me">
-				<div class="entete">
-					<h3>10:12AM, Today</h3>
-					<h2>Vincent</h2>
-					<span class="status blue"></span>
-				</div>
-				<div class="triangle"></div>
-				<div class="message">
-					OK
-				</div>
-			</li>
+		<ul className="chat-area" id="chat">
+			{messages.map((msg)=>{
+				if(msg.id===JSON.parse(localStorage.getItem('adminData')).adminid)
+				return <div className="chat-message-div"> 
+                <span style={{flexGrow:'1'}}></span>
+                <div className='chat-message-sent'>{msg.msg}</div>
+             </div>
+			 else
+			 return <div className="chat-message-div"> 
+			 <div className='chat-message-received '>{msg.msg}</div>
+			 <span style={{flexGrow:'1'}}></span>
+		  </div>
+			})}
 		</ul>
 		<footer>
-			<input placeholder="Type your message"></input>
+			<input placeholder="Type your message" id="message" onKeyUp={(e)=>handleenter(e)}></input>
 			<button onClick={handlesendmessage} >Send</button>
 		</footer>
 	</main>
